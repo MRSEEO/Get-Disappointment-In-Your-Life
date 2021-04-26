025a63effbf692a0de6055600dbaf9c55ddcfcca
letofagbot <mr.seeo@mail.ru>
2021-04-27 00:48:44 +0300

    hui v ochke
    Великое обновление на великий и могучий язык. афу

--- main.gd
+++ main.gd
@@ -1,9 +1,9 @@
 extends Control
 
 var list = {
-	"name" : ["asd", "asss", "asdasd"],
-	"series" : [20, 23, 5],
-	"duration" : [10, 5, 1],
+	"name" : [],
+	"series" : [],
+	"duration" : [],
 	"one_anime" : [],
 }
 
@@ -11,6 +11,12 @@ var series = 0
 var full_time = 0
 
 func _ready():
+	var save_game = File.new()
+	if not save_game.file_exists("res://list.save"):
+		return
+	
+	save_game.open("res://list.save", File.READ)
+	list = parse_json(save_game.get_as_text())
 	
 	#MAIN
 	#Число, сколько имён.
@@ -20,7 +26,7 @@ func _ready():
 		$scroll/lists/c_duration/duration.add_item(str(list.duration[a]))
 		
 		###Считаем время одного аниме.
-		list.one_anime.append(stepify(list.series[a] * list.duration[a] / 60, 0.1)) #Округляем.
+		list.one_anime.append(stepify(list.series[a] * list.duration[a] / 60, 0.01)) #Округляем.
 		
 		$scroll/lists/c_full_anime/full_anime.add_item(str(list.one_anime[a]))
 		
@@ -29,12 +35,12 @@ func _ready():
 	
 	#/// #Число серий.
 	for a in list.series.size():
-		series = list.series[0] + list.series[a]
+		series +=list.series[a]
 		$text/series.set_text(str(series))
 		
 	#/// #Полное время.
 	for a in list.one_anime.size():
-		full_time = list.one_anime[0] + list.one_anime[a]
+		full_time += list.one_anime[a]
 		$text/total_hours.set_text(str(full_time))
 
 
@@ -76,9 +82,43 @@ func _on_del_pressed():
 		
 		for a in $scroll/lists/c_names/names.get_selected_items().size():
 			var del = $scroll/lists/c_names/names.get_selected_items()
-			$scroll/lists/c_names/names.remove_item(del[a]); $scroll/lists/help.remove_item(del[a])
-			$scroll/lists/c_series/series.remove_item(del[a])
-			$scroll/lists/c_duration/duration.remove_item(del[a])
-			$scroll/lists/c_full_anime/full_anime.remove_item(del[a])
+			#Удаление. (Удали из листа)
+				
+				#Должно быть удаление из массива.
+			list.name.remove(del[0])
+			list.series.remove(del[0])
+			list.duration.remove(del[0])
+			list.one_anime.remove(del[0])
+			
+			$scroll/lists/c_names/names.remove_item(del[0]); $scroll/lists/help.remove_item(del[0])
+			$scroll/lists/c_series/series.remove_item(del[0])
+			$scroll/lists/c_duration/duration.remove_item(del[0])
+			$scroll/lists/c_full_anime/full_anime.remove_item(del[0])
+			print(list)
+			
+		#Пересчитываем говно из жопы.
+		full_time = 0
+		for t in list.one_anime.size():
+			full_time += list.one_anime[t]	
+		$text/total_hours.set_text(str(full_time))
+		
+		series = 0
+		for s in list.series.size():
+			series += list.series[s]
+		$text/series.set_text(str(series))
+		
+			#/// #Число анимешек.
+		$text/animes.set_text(str($scroll/lists/c_names/names.get_item_count()))
+			
+
 	else:
 		print("no")
+
+
+func _on_save_pressed():
+	var save_game = File.new()
+	save_game.open("res://list.save", File.WRITE)
+		
+	save_game.store_line(to_json(list))
+	save_game.close()
+	$Save.popup()
